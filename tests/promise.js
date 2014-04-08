e92e909ac20f8a3e54ebbe4132169b05f82038fa
Mond Wan <mondwan.1015@gmail.com>
2014-04-09 00:30:36 +0800

    Get latest ssh2 version and modify script for testing

--- tests/promise.js
+++ tests/promise.js
@@ -6,14 +6,23 @@ var c = new ssh();
 
 var send = function (cmd) {
     var deferred = Q.defer();
+    var deferred2 = Q.defer();
 
     c.on("ready", function () {
         c.exec(cmd, function (err, stream) {
             if (err) {
-                deferred.resolve(err);
+                deferred.reject(err);
             }
             stream.on("data", function (data) {
-                deferred.resolve(data);
+                var decoder = new StringDecoder('utf8');
+                var output = decoder.write(data);
+                deferred.resolve(output);
+            });
+            stream.on("exit", function (code, signal) {
+                deferred2.resolve({
+                    code: code,
+                    signal: signal
+                });
             });
         });
     });
@@ -22,47 +31,95 @@ var send = function (cmd) {
         deferred.reject(err);
     });
 
-    c.on("end", function (err) {
-        deferred.resolve(true);
-    });
+    return deferred.promise
+        .then(function (res) {
+            return deferred2.promise
+                .then(function (obj) {
+                    obj.result = res;
+                    return obj;
+                });
+        });
+};
 
-    c.on("close" , function (err) {
-        if (err === true) {
-            deferred.reject(new Error("Socket waas closed due to errors")); 
+var disconnect = function () {
+    var deferred = Q.defer();
+
+    c.on("close", function (err) {
+        console.log("disconnect");
+        if (err) {
+            deferred.reject(err);
         } else {
             deferred.resolve(true);
         }
     });
-
+    c.end();
     return deferred.promise;
 };
 
 c.connect({
     host: 'localhost',
     port: 22,
-    username: 'codingFarmer',
-    password: "codingFarmer"
+    username: 'mondwan',
+    //debug: console.log,
+    password: "mondwan"
 });
 
-send("uptime").then(function (res) {
-    console.log("then:res", res);
-    var decoder = new StringDecoder('utf8');
-    var output = decoder.write(res);
-    console.log("then:out", output);
-    return output;
-}).catch(function (err) {
-    console.log("catch:err", err);
-}).finally(function () {
-    console.log("finally");
-    c.end();
-}).done();
+send("date")
+    .then(function (res) {
+        console.log("1st date", res);
+        return res;
+    })
+    //.finally(disconnect)
+    .done();
+send("uptime")
+    .then(function (res) {
+        console.log("2nd uptime", res);
+        return res;
+    })
+    //.finally(disconnect)
+    .done();
 
-
-/* example output:
- * codingFarmer@codingFarmer-P61-USB3P:~/Documents/git/js-ssh2-promise/tests$ node promise.js 
- * then:res <Buffer 20 32 33 3a 35 39 3a 30 34 20 75 70 20 20 32 3a 33 37 2c 20 20 32 20 75 73 65 72 73 2c 20 20 6c 6f 61 64 20 61 76 65 72 61 67 65 3a 20 30 2e 34 31 2c 20 ...>
- * then:out  23:59:04 up  2:37,  2 users,  load average: 0.41, 0.43, 0.50
- *
- * finally
- * codingFarmer@codingFarmer-P61-USB3P:~/Documents/git/js-ssh2-promise/tests$ fg
- * */
+send("date")
+    .then(function (res) {
+        console.log("3rd date", res);
+        return res;
+    })
+    //.finally(disconnect)
+    .done();
+send("uptime")
+    .then(function (res) {
+        console.log("4th uptime", res);
+        return res;
+    })
+    .finally(disconnect)
+    .done();
+send("date")
+    .then(function (res) {
+        console.log("5th date", res);
+        return res;
+    })
+    //.finally(disconnect)
+    .done();
+send("uptime")
+    .then(function (res) {
+        console.log("6th uptime", res);
+        return res;
+    })
+    //.finally(disconnect)
+    .done();
+send("date")
+    .then(function (res) {
+        console.log("7th date", res);
+        return res;
+    })
+    //.finally(disconnect)
+    .done();
+send("uptime")
+    .then(function (res) {
+        console.log("8th uptime", res);
+        return res;
+    })
+    .catch(function (err) {
+        console.log(err);
+    })
+    .done();
